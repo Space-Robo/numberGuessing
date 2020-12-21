b42481e17e5b80586050ebe93c7708171103b128
Yash Raj <yash2raj.yr@gmail.com>
2020-12-21 17:34:58 +0000

    Added descriptions

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 import random
 
-'''
-Python Project: Number Guessing
-
-About:
-Number Guessing is a very interesting and fun game among a lot of people. 
-The computer will randomly select a number from a range of numbers provided by the user.
-The user will also provide how many chances they need to guess the number.
-As the user is attempting to guess the number, the computer will indicate whether the number guessed is greater than, less than, or equal to the number selected by the computer.
-If the user runs out of chances, it will let them know that the game is over and it will reveal the number selected.
-'''
-
-def pickNumberFromRange(start, end):
-  return random.randint(start, end)
-
 def run():
   gettingStartNum = True
   gettingEndNum = True
@@ -23,6 +9,7 @@ def run():
   print("You will need to provide two numbers to create a range of numbers the computer needs to select from.")
   print("For example, if you put 1 as your start number and 100 as your end number, the computer will pick a number between 1 and 100.\n")
   
+  # getting the start value of the range (making sure it's an integer)
   while gettingStartNum:
     try:
       startNum = int(input("Enter your start number: "))
@@ -30,6 +17,7 @@ def run():
     except ValueError:
       print("Please enter an integer. Try again...")
   
+  # getting the end value of the range (making sure it's an integer)
   while gettingEndNum:
     try:
       endNum = int(input("Enter your end number: "))
@@ -39,6 +27,7 @@ def run():
 
   print('The computer has selected a number bewteen ' + str(startNum) + ' and ' + str(endNum))
 
+  # getting the number of tries the user wants (making sure it's an integer)
   while gettingNumTries:
     try:
       numChances = int(input("How many tries would you like to guess the number?: "))
@@ -46,12 +35,14 @@ def run():
     except ValueError:
       print("Please enter an integer. Try again...")
   
-  randomNum = pickNumberFromRange(startNum, endNum)
+  # Picking a random number within the range provided
+  randomNum = random.randint(startNum, endNum)
 
   while numChances > 0:
     gettingUserGuess = True
     print("")
     print('Chances Left: ' + str(numChances))
+    # User's guess (making sure it's an integer)
     while gettingUserGuess:
       try:
         guess = int(input("Enter your guess: "))
@@ -59,18 +50,22 @@ def run():
       except ValueError:
         print("Please enter an integer. Try again...")
 
-    if guess == randomNum:
+    if guess == randomNum: # if guess matches the selected number, set numchances to -1
       numChances = -1
-    elif guess > randomNum:
+
+    elif guess > randomNum: # if guess is greater than the selected number, let the user know
       print("The number selected is less than " + str(guess) + '.')
       numChances += -1
-    elif guess < randomNum:
+
+    elif guess < randomNum: # if guess is greater than the selected number, let the user know
       print("The number selected is greater than " + str(guess) + '.')
       numChances += -1
   
+  # When numChances equals -1, the computer knows the user has guessed the number
   if numChances == -1:
     print('Congratulations! You guessed the number '+ str(randomNum) + '!')
   
+  # If numChances has reached 0, the game ends and the number is revealed
   else:
     print('You failed to guess the number ' + str(randomNum) + ' under the number of chances provided.')
 
